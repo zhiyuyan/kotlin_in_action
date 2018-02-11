dc5d0ae9347332852d0f174aa8203d05f1f7814d
yanzy <1187997355@qq.com>
2018-02-11 22:56:05 +0800

    chapter3

--- src/chapter3/local_functions/Main.kt
+++ src/chapter3/local_functions/Main.kt
@@ -8,14 +8,13 @@ class User(val id: Int, val name: String, val address: String)
 
 fun saveUser(user: User) {
 
-    fun validate(user: User,
-                 value: String,
+    fun validate(value: String,
                  fieldName: String) {
         if (value.isEmpty()) {
             throw IllegalArgumentException("Cannot save user ${user.id}: $fieldName is empty")
         }
     }
 
-    validate(user, user.name, "Name")
-    validate(user, user.address, "Address")
+    validate(user.name, "Name")
+    validate(user.address, "Address")
 }
